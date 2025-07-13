9239edd1a4343f284b2f77193f0b100a1f529516
PROOTAKU2 <prootaku99963@gmail.com>
2025-07-14 02:02:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","ALlNAOWNER")
+OWNER_USERNAME = getenv("OWNER_USERNAME","iarshop")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME","alinamusicop_RcBot")
+BOT_USERNAME = getenv("BOT_USERNAME","kulfixmusic_bot")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME")
 # ---------------------------------------------------------
@@ -30,7 +30,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", -1002847152143))
 
 # Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 8103548616))
+OWNER_ID = int(getenv("OWNER_ID", 7692684154))
 
 
 # make your bots privacy from telegra.ph and put your url here 
